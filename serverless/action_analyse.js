e05a70c577c8605882b49f61573c3e79709ed906
Iwan Winoto <iwinoto@gmail.com>
2016-07-08 10:17:40 +1000

    Analysis direct to watson-developer-cloud

--- serverless/action_analyse.js
+++ serverless/action_analyse.js
@@ -3,26 +3,30 @@ function main(msg){
   //http request needs to block because main will return control to OpenWhisk
   //as soon as req.end() is called and callback on 'end' event will not be called.
   //Hence we use whiks.async() and whisk.done()
-  var request = require('request');
   var result = "";
+  var watson = require('watson-developer-cloud');
+  var personality_insights = watson.personality_insights({
+    username: 'f75cdd46-5b62-4b30-83a5-c43bc8a72079',
+    password: '5S3ySGflKKB3',
+    version: 'v2'
+  });
 
-  var requestOpts = {
-    baseUrl: 'https://iw-personalityinsights-demo.mybluemix.net',
-    port: 80,
-    uri: '/demo',
-    method: 'POST',
-    form: { text: msg.text, language: msg.language }
+  var params = {
+    text: msg.text,
+    language: (msg.language || "en"),
+    contentType: "text/plain; charset=utf-8"
   };
 
-  request.post(requestOpts, function(err, res, body){
-    if(err){
-      result = err.message;
-    } else {
-      result = body;
-    };
-    whisk.done({ analysis: result })
-  });
-/* End using request */
+  personality_insights.profile(params, function(error, response) {
+      if (error){
+        console.log('Watson Personality Insights error: ', error);
+        result = error;
+      } else {
+        result = response;
+      };
+      whisk.done({ analysis: result })
+    }
+  );
 
   return whisk.async();
 }
